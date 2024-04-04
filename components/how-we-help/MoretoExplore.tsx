46701fa2cf19bbe508303a241326642b5b19df76
Ayim William <odhisayim@gmail.com>
2024-04-04 10:15:17 +0300

    Fixed the font for moretoexplore how we help

--- components/how-we-help/MoretoExplore.tsx
+++ components/how-we-help/MoretoExplore.tsx
@@ -14,7 +14,7 @@ const MoretoExplore = ({}) => {
               More to explore
             </div>
             <div className="self-stretch  font-bold leading-10 text-center text-primarymd:text-2xl">
-              <p className=" my-3 font-bold leading-10 text-center text-primary md:text-2xl">
+              <p className=" my-3 font-normal leading-10 text-center text-primary md:text-xl">
               Contact us using the contact details above and we’ll be happy to help. Or check out the following pages to learn more about how TYC Africa Trust can help you.
               </p>
             </div>
@@ -23,7 +23,7 @@ const MoretoExplore = ({}) => {
         
         <div className="grid items-start justify-start gap-5 py-5 md:gap-24 md:grid-cols-2 md:py-20 px-14">
           <div className="relative rounded-md w-96 h-36 bg-primary text-primary-foreground hover:bg-primary/90 transition-transform duration-1000 transform -skew-x-12 gap-1 shadow-[10px_8px_0px_rgba(29,78,216,1)] hover:shadow-[15px_8px_0px_rgba(29,78,216,1)]">
-            <div className="w-80 h-16 left-[19px] top-[58px] absolute  text-4xl font-semibold  leading-10 tracking-wide">
+            <div className="w-80 h-16 left-[19px] top-[58px] absolute  text-3xl font-normal  leading-10 tracking-wide">
             <Link href={'/partner'}>Partner with us</Link>
             </div>
             <div className="w-44 h-36 left-[274px] top-0 absolute opacity-70" />
@@ -31,7 +31,7 @@ const MoretoExplore = ({}) => {
 
 
           <div className="relative rounded-md w-96 h-36 bg-primary text-primary-foreground hover:bg-primary/90 transition-transform duration-1000 transform -skew-x-12 gap-1 shadow-[10px_8px_0px_rgba(29,78,216,1)] hover:shadow-[15px_8px_0px_rgba(29,78,216,1)]">
-            <div className="w-80 h-16 left-[19px] top-[58px] absolute  text-4xl font-semibold  leading-10 tracking-wide">
+            <div className="w-80 h-16 left-[19px] top-[58px] absolute  text-3xl font-normal  leading-10 tracking-wide">
               <Link href={'/resources-at-tyc-africa-trust'}>Knowledge hub</Link>
             </div>
             <div className="w-44 h-36 left-[274px] top-0 absolute opacity-70" />
@@ -39,7 +39,7 @@ const MoretoExplore = ({}) => {
 
 
           <div className="relative rounded-md w-96 h-36 bg-primary text-primary-foreground hover:bg-primary/90 transition-transform duration-1000 transform -skew-x-12 gap-1 shadow-[10px_8px_0px_rgba(29,78,216,1)] hover:shadow-[15px_8px_0px_rgba(29,78,216,1)]">
-            <div className="w-80 h-16 left-[19px] top-[58px] absolute  text-4xl font-semibold  leading-10 tracking-wide">
+            <div className="w-80 h-16 left-[19px] top-[58px] absolute  text-3xl font-normal  leading-10 tracking-wide">
               Refer a young person
             </div>
             <div className="w-44 h-36 left-[274px] top-0 absolute opacity-70" />
@@ -47,7 +47,7 @@ const MoretoExplore = ({}) => {
 
           
           <div className="relative rounded-md w-96 h-36 bg-primary text-primary-foreground hover:bg-primary/90 transition-transform duration-1000 transform -skew-x-12 gap-1 shadow-[10px_8px_0px_rgba(29,78,216,1)] hover:shadow-[15px_8px_0px_rgba(29,78,216,1)]">
-            <div className="w-80 h-16 left-[19px] top-[58px] absolute  text-4xl font-semibold  leading-10 tracking-wide">
+            <div className="w-80 h-16 left-[19px] top-[58px] absolute  text-3xl font-normal  leading-10 tracking-wide">
             <Link href={'/frequently-asked-questions'}>Frequently Asked Questions</Link>
             </div>
             <div className="w-44 h-36 left-[274px] top-0 absolute opacity-70" />
