ccfd6a29c25c23310ee1440cca080ef49e42fc69
Ayim William <odhisayim@gmail.com>
2024-04-04 12:39:25 +0300

    added google maps

--- app/contact-us/page.tsx
+++ app/contact-us/page.tsx
@@ -26,6 +26,25 @@ export default function Home() {
                 </h2>
                 <div className="md:flex justify-between items-center">
                     <div className="w-full md:w-1/2 h-[300px] sm:h-[450px]">
+                        <iframe
+                            title="google-maps"
+                            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d63823.92673525492!2d36.95404762190003!3d-1.1637023329624467!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x182f38a78cb4e50d%3A0xdaa7fad9c2cbf0fb!2sRuiru!5e0!3m2!1sen!2ske!4v1712223081509!5m2!1sen!2ske"
+                            className="border-0 w-full h-full"
+                            allowFullScreen=""
+                            loading="lazy"
+                            referrerPolicy="no-referrer-when-downgrade"
+                        ></iframe>
+
+                    </div>
+                    <div className="w-full mt-8 md:mt-0 md:w-1/2 sm:h-[450px] lg:flex items-center bg-indigo-100 px-4 lg:px-8 py-8 pl-7">
+                        <form className="w-full" onSubmit={handleSubmit}>
+                            <div className="mb-5">
+                                <input 
+                                    
+                                />
+                            </div>
+
+                        </form>
 
                     </div>
 
