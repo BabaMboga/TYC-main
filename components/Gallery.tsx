df4eb3f39ddae4e771a0cbf60099c2140febb78c
Ayim William <odhisayim@gmail.com>
2024-04-04 18:19:20 +0300

    Changed code for slideshow

--- components/Gallery.tsx
+++ components/Gallery.tsx
@@ -16,7 +16,48 @@
 
 //     return (
 //         <Slider {...settings}>
-
+//             <div>
+//                 <img />
+//                 <p>Caption 1</p>
+//             </div>
+//             <div>
+//                 <img />
+//                 <p>Caption 1</p>
+//             </div>
+//             <div>
+//                 <img />
+//                 <p>Caption 1</p>
+//             </div>
+//                         <div>
+//                 <img />
+//                 <p>Caption 1</p>
+//             </div>
 //         </Slider>
 //     )
 // }
+
+import React from 'react';
+import { Carousel } from 'react-responsive-carousel';
+import 'react-responsive-carousel/lib/styles/carousel.min.css'; // Import carousel styles
+
+const Gallery: React.FC = () => {
+  return (
+    <Carousel>
+      <div>
+        <img src="/image1.jpg" alt="Image 1" />
+        <p className="legend">Caption 1</p>
+      </div>
+      <div>
+        <img src="/image2.jpg" alt="Image 2" />
+        <p className="legend">Caption 2</p>
+      </div>
+      <div>
+        <img src="/image3.jpg" alt="Image 3" />
+        <p className="legend">Caption 3</p>
+      </div>
+      {/* Add more images as needed */}
+    </Carousel>
+  );
+};
+
+export default Gallery;
