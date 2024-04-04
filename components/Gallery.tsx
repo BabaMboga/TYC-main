8ccee523e92ad82fc093bdf27cba15afecb4d3c9
Ayim William <odhisayim@gmail.com>
2024-04-04 17:59:56 +0300

    Added gallery code but there is an issue

---
+++ components/Gallery.tsx
@@ -0,0 +1,22 @@
+// import React from 'react';
+// import Slider from 'react-slick';
+// import 'slick-carousel/slick/slick.css';
+// import 'slick-carousel/slick/slick-them.css';
+
+// const Gallery: React.FC = () => {
+//     const settings = {
+//         dots: true,
+//         infinite: true,
+//         speed: 300,
+//         slidesToShow: 1,
+//         slidesToScroll: 1,
+//         autoplay: true,
+//         autoplaySpeed: 2000,
+//     };
+
+//     return (
+//         <Slider {...settings}>
+
+//         </Slider>
+//     )
+// }
