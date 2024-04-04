5f1faafc618aa98dd9137aad3b5447009350d20d
Ayim William <odhisayim@gmail.com>
2024-04-04 10:36:55 +0300

    finished opportunitiescard/support-us

--- components/support-us/OpportunitiesCard1.tsx
+++ components/support-us/OpportunitiesCard1.tsx
@@ -22,19 +22,19 @@ const OpportunitiesCard1 = ({}) => {
                 height: '400px',}}></Image>
               <div className="flex flex-col items-start self-stretch justify-start gap-4 px-4 pt-6 pb-4">
                 <div className="flex flex-col items-start justify-start gap-1 ">
-                  <div className="self-stretch  text-[40px] font-bold  leading-[44px]">
+                  <div className="self-stretch  text-[30px] font-semibold  leading-[34px]">
                     Volunteer your time
                   </div>
                 </div>
-                <div className="self-stretch text-2xl font-normal  leading-[33.60px]">
+                <div className="self-stretch text-base font-normal  leading-[33.60px]">
                 Give the gift of your time. Join our team of passionate volunteers and help us create a better world through your actions and dedication.
                 </div>
               </div>
               <div className=" gap-4 p-4 ">
                 <div className=" gap-4 py-4 pr-2 ">
                   <div className=" ">
-                    <div className="text-xl font-medium leading-tight tracking-wide underline flex justify-between w-full">
-                     <Link href={'/'}>Be a catalyst for change</Link> <Icons.right/>
+                    <div className="text-base font-medium leading-tight tracking-wide underline flex justify-between w-full">
+                     <Link className="flex" href={'/'}>Be a catalyst for change <Icons.right/></Link> 
                     </div>
                   </div>
                   <div className="relative w-6 h-6" />
@@ -51,19 +51,19 @@ const OpportunitiesCard1 = ({}) => {
                 height: '400px',}}></Image>
               <div className="flex flex-col items-start self-stretch justify-start gap-4 px-4 pt-6 pb-4">
                 <div className="flex flex-col items-start justify-start gap-1 ">
-                  <div className="self-stretch  text-[40px] font-bold  leading-[44px]">
+                  <div className="self-stretch  text-[30px] font-semibold  leading-[34px]">
                     Fundraise with us
                   </div>
                 </div>
-                <div className="self-stretch text-2xl font-normal  leading-[33.60px]">
+                <div className="self-stretch text-base font-normal  leading-[33.60px]">
                 Join hands with us to raise funds for critical initiatives. Be a part of our fundraising efforts and help us make a difference.
                 </div>
               </div>
               <div className=" gap-4 p-4 ">
                 <div className=" gap-4 py-4 pr-2 ">
                   <div className=" ">
-                    <div className="text-xl font-medium leading-tight tracking-wide underline flex justify-between w-full">
-                     <Link href={'/'}>Help amplify our impact</Link> <Icons.right/>
+                    <div className="text-base font-medium leading-tight tracking-wide underline flex justify-between w-full">
+                     <Link className="flex" href={'/'}>Help amplify our impact <Icons.right/></Link> 
                     </div>
                   </div>
                   <div className="relative w-6 h-6" />
@@ -80,18 +80,18 @@ const OpportunitiesCard1 = ({}) => {
                 height: '400px',}}></Image>
               <div className="flex flex-col items-start self-stretch justify-start gap-4 px-4 pt-6 pb-4">
                 <div className="flex flex-col items-start justify-start gap-1 ">
-                  <div className="self-stretch  text-[40px] font-bold  leading-[44px]">
+                  <div className="self-stretch  text-[30px] font-semibold  leading-[34px]">
                     Leave a gift in your will
                   </div>
                 </div>
-                <div className="self-stretch text-2xl font-normal  leading-[33.60px]">
+                <div className="self-stretch text-base font-normal  leading-[33.60px]">
                 Secure the future of the causes you care about by leaving a legacy gift in your will. Ensure that your values live on and continue to make a difference.                </div>
               </div>
               <div className=" gap-4 p-4 ">
                 <div className=" gap-4 py-4 pr-2 ">
                   <div className=" ">
-                    <div className="text-xl font-medium leading-tight tracking-wide underline flex justify-between w-full">
-                     <Link href={'/'}>Leave a lasting legacy</Link> <Icons.right/>
+                    <div className="text-base font-medium leading-tight tracking-wide underline flex justify-between w-full">
+                     <Link className="flex" href={'/'}>Leave a lasting legacy <Icons.right/></Link> 
                     </div>
                   </div>
                   <div className="relative w-6 h-6" />
@@ -108,18 +108,18 @@ const OpportunitiesCard1 = ({}) => {
                 height: '400px',}}></Image>
               <div className="flex flex-col items-start self-stretch justify-start gap-4 px-4 pt-6 pb-4">
                 <div className="flex flex-col items-start justify-start gap-1 ">
-                  <div className="self-stretch  text-[40px] font-bold  leading-[44px]">
+                  <div className="self-stretch  text-[30px] font-bold  leading-[34px]">
                     Join our campaigns
                   </div>
                 </div>
-                <div className="self-stretch text-2xl font-normal  leading-[33.60px]">
+                <div className="self-stretch text-base font-normal  leading-[33.60px]">
                 Be part of our journey for change. Discover our impactful campaigns and find your place in our mission for a better world.             </div>
               </div>
               <div className=" gap-4 p-4 ">
                 <div className=" gap-4 py-4 pr-2 ">
                   <div className=" ">
-                    <div className="text-xl font-medium leading-tight tracking-wide underline flex justify-between w-full">
-                     <Link href={'/'}>Join our cause</Link> <Icons.right/>
+                    <div className="text-base font-medium leading-tight tracking-wide underline flex justify-between w-full">
+                     <Link className="flex" href={'/'}>Join our cause <Icons.right/></Link> 
                     </div>
                   </div>
                   <div className="relative w-6 h-6" />
