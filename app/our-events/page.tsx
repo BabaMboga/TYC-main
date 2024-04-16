59d86ce4ea0b85a5b40be22c4b2cfc22eb9979e8
Ayim William <odhisayim@gmail.com>
2024-04-16 16:44:01 +0300

    Events page formatted

--- app/our-events/page.tsx
+++ app/our-events/page.tsx
@@ -1,21 +1,32 @@
-'use client'
+"use client";
 import Image from "next/image";
 import { FC } from "react";
 import { Button } from "@/components/ui/button";
 import { Icons } from "@/components/Icons";
 import Link from "next/link";
-import Calendar  from '@fullcalendar/react';
-import dayGridPlugin from '@fullcalendar/daygrid';
-import {Tooltip} from 'react-tooltip';
+import Calendar from "@fullcalendar/react";
+import dayGridPlugin from "@fullcalendar/daygrid";
+import { Tooltip } from "react-tooltip";
 
 const Home = ({}) => {
-
   //Array of Calendar events
 
   const EventDates = [
-    {title: 'Conference Highlight', date: '2024-04-05', description: 'Event: Conference Highlight'},
-    {title: 'Workshop SChedule', date: '2024-04-15', description: 'Event: Workshop Schedule'},
-    {title: 'Community Outreach ', date: '2024-04-25', description: 'Event: Community Outreach Program'}
+    {
+      title: "Conference Highlight",
+      date: "2024-04-05",
+      description: "Event: Conference Highlight",
+    },
+    {
+      title: "Workshop SChedule",
+      date: "2024-04-15",
+      description: "Event: Workshop Schedule",
+    },
+    {
+      title: "Community Outreach ",
+      date: "2024-04-25",
+      description: "Event: Community Outreach Program",
+    },
   ];
 
   return (
@@ -37,14 +48,17 @@ const Home = ({}) => {
               </div>
               <div className="w-full" id="calendar">
                 <Calendar
-                  plugins={[ dayGridPlugin]}
+                  plugins={[dayGridPlugin]}
                   initialView="dayGridMonth"
-                  events={EventDates.map(event => ({
+                  events={EventDates.map((event) => ({
                     ...event,
-                    description: `Event: ${event.title}`
-                  }))} 
-                  eventContent={({event}) => (
-                    <span date-tip={event.extendedProps.description} data-for="event-tooltip">
+                    description: `Event: ${event.title}`,
+                  }))}
+                  eventContent={({ event }) => (
+                    <span
+                      date-tip={event.extendedProps.description}
+                      data-for="event-tooltip"
+                    >
                       {event.title}
                     </span>
                   )}
@@ -80,9 +94,13 @@ const Home = ({}) => {
                   </div>
                 </div>
                 <div className="self-stretch text-2xl font-normal  leading-[33.60px]">
-                Come and join the TYC as we visit the LITTLE ANGELS CENTER for Orphans and Vulnerable Children, 
-                where we shall spend a day filled with love, laughter, and joy. Together, we'll share unforgettable moments with the children, 
-                bringing smiles to their faces and warmth to their hearts. Let's make a difference in their lives and create cherished memories that will last a lifetime.
+                  Come and join the TYC as we visit the LITTLE ANGELS CENTER for
+                  Orphans and Vulnerable Children, where we shall spend a day
+                  filled with love, laughter, and joy. Together, we'll share
+                  unforgettable moments with the children, bringing smiles to
+                  their faces and warmth to their hearts. Let's make a
+                  difference in their lives and create cherished memories that
+                  will last a lifetime.
                 </div>
               </div>
               <div className=" gap-4 p-4 ">
@@ -93,11 +111,10 @@ const Home = ({}) => {
                         href={"/"}
                         className="flex flex-row justify-between w-full"
                       >
-                        <Button className='flex justify-betweenpy-4 px-8 transition-transform duration-1000 transform -skew-x-12 gap-1 shadow-[10px_8px_0px_rgba(29,78,216,1)] hover:shadow-[15px_8px_0px_rgba(29,78,216,1)]'>
+                        <Button className="flex justify-betweenpy-4 px-8 transition-transform duration-1000 transform -skew-x-12 gap-1 shadow-[10px_8px_0px_rgba(29,78,216,1)] hover:shadow-[15px_8px_0px_rgba(29,78,216,1)]">
                           <span>Register</span>
-                          <Icons.right/>
+                          <Icons.right />
                         </Button>
-
                       </Link>
                     </div>
                   </div>
@@ -127,11 +144,14 @@ const Home = ({}) => {
                   </div>
                 </div>
                 <div className="self-stretch text-2xl font-normal  leading-[33.60px]">
-                  Join us for a day of environmental stewardship as we embark on a tree planting initiative. 
-                  Together, we'll nurture nature by planting trees, breathing life into our planet, and creating a greener, 
-                  more sustainable future for generations to come. Let's roll up our sleeves, dig into the soil, 
-                  and sow the seeds of change. Every tree we plant is a step towards a healthier, more vibrant ecosystem, 
-                  and with your help, we can make a lasting impact on the world around us.
+                  Join us for a day of environmental stewardship as we embark on
+                  a tree planting initiative. Together, we'll nurture nature by
+                  planting trees, breathing life into our planet, and creating a
+                  greener, more sustainable future for generations to come.
+                  Let's roll up our sleeves, dig into the soil, and sow the
+                  seeds of change. Every tree we plant is a step towards a
+                  healthier, more vibrant ecosystem, and with your help, we can
+                  make a lasting impact on the world around us.
                 </div>
               </div>
 
@@ -143,11 +163,10 @@ const Home = ({}) => {
                         href={"/"}
                         className="flex flex-row justify-between w-full"
                       >
-                        <Button className='flex justify-betweenpy-4 px-8 transition-transform duration-1000 transform -skew-x-12 gap-1 shadow-[10px_8px_0px_rgba(29,78,216,1)] hover:shadow-[15px_8px_0px_rgba(29,78,216,1)]'>
+                        <Button className="flex justify-betweenpy-4 px-8 transition-transform duration-1000 transform -skew-x-12 gap-1 shadow-[10px_8px_0px_rgba(29,78,216,1)] hover:shadow-[15px_8px_0px_rgba(29,78,216,1)]">
                           <span>Register</span>
-                          <Icons.right/>
+                          <Icons.right />
                         </Button>
-
                       </Link>
                     </div>
                   </div>
@@ -177,11 +196,14 @@ const Home = ({}) => {
                   </div>
                 </div>
                 <div className="self-stretch text-2xl font-normal  leading-[33.60px]">
-                Embrace the opportunity to bring warmth and companionship to the elderly residents of our community 
-                through our Elderly Care Visit program. As we step into their homes, we step into their stories, sharing 
-                laughter, memories, and heartfelt conversations. Let's create a space where every interaction is a 
-                moment of connection, where smiles are exchanged, and hearts are lifted. Together, let's honor their 
-                wisdom, celebrate their lives, and cultivate a bond that transcends generations.
+                  Embrace the opportunity to bring warmth and companionship to
+                  the elderly residents of our community through our Elderly
+                  Care Visit program. As we step into their homes, we step into
+                  their stories, sharing laughter, memories, and heartfelt
+                  conversations. Let's create a space where every interaction is
+                  a moment of connection, where smiles are exchanged, and hearts
+                  are lifted. Together, let's honor their wisdom, celebrate
+                  their lives, and cultivate a bond that transcends generations.
                 </div>
               </div>
               <div className=" gap-4 p-4 ">
@@ -192,12 +214,10 @@ const Home = ({}) => {
                         href={"/"}
                         className="flex flex-row justify-between w-full"
                       >
-                        
-                        <Button className='flex justify-betweenpy-4 px-8 transition-transform duration-1000 transform -skew-x-12 gap-1 shadow-[10px_8px_0px_rgba(29,78,216,1)] hover:shadow-[15px_8px_0px_rgba(29,78,216,1)]'>
+                        <Button className="flex justify-betweenpy-4 px-8 transition-transform duration-1000 transform -skew-x-12 gap-1 shadow-[10px_8px_0px_rgba(29,78,216,1)] hover:shadow-[15px_8px_0px_rgba(29,78,216,1)]">
                           <span>Register</span>
-                          <Icons.right/>
+                          <Icons.right />
                         </Button>
-
                       </Link>
                     </div>
                   </div>
@@ -227,12 +247,16 @@ const Home = ({}) => {
                   </div>
                 </div>
                 <div className="self-stretch text-2xl font-normal  leading-[33.60px]">
-                  Get ready to roll up your sleeves and make a difference in our community with our Community Care 
-                  Initiative. Together, we'll come together to beautify our streets, rejuvenate our public spaces, 
-                  and create a cleaner, healthier environment for everyone to enjoy. From picking up litter to planting 
-                  flowers, every small act of kindness contributes to a more vibrant and welcoming community. Let's join 
-                  hands, harness the power of unity, and show our love and care for the places we call home.              </div>
+                  Get ready to roll up your sleeves and make a difference in our
+                  community with our Community Care Initiative. Together, we'll
+                  come together to beautify our streets, rejuvenate our public
+                  spaces, and create a cleaner, healthier environment for
+                  everyone to enjoy. From picking up litter to planting flowers,
+                  every small act of kindness contributes to a more vibrant and
+                  welcoming community. Let's join hands, harness the power of
+                  unity, and show our love and care for the places we call home.{" "}
                 </div>
+              </div>
               <div className=" gap-4 p-4 ">
                 <div className=" h-12 py-4 pr-2 grow shrink basis-0">
                   <div className="">
@@ -241,12 +265,10 @@ const Home = ({}) => {
                         href={"/"}
                         className="flex flex-row justify-between w-full"
                       >
-                        
-                        <Button className='flex justify-betweenpy-4 px-8 transition-transform duration-1000 transform -skew-x-12 gap-1 shadow-[10px_8px_0px_rgba(29,78,216,1)] hover:shadow-[15px_8px_0px_rgba(29,78,216,1)]'>
+                        <Button className="flex justify-betweenpy-4 px-8 transition-transform duration-1000 transform -skew-x-12 gap-1 shadow-[10px_8px_0px_rgba(29,78,216,1)] hover:shadow-[15px_8px_0px_rgba(29,78,216,1)]">
                           <span>Register</span>
-                          <Icons.right/>
+                          <Icons.right />
                         </Button>
-
                       </Link>
                     </div>
                   </div>
