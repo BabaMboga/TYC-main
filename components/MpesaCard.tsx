b86908880ffbe7ddb9910342b2099c826d0a9503
Ayim William <odhisayim@gmail.com>
2024-04-20 23:19:15 +0300

    Finished general setup

---
+++ components/MpesaCard.tsx
@@ -0,0 +1,19 @@
+const MpesaCard = () => {
+    return (
+        <>
+            <div className="bg-green-500 text-white text-center py-4 paybill-box">
+                <div className="flex justify-center mb-4">
+                    <div className="text-left mr-6">
+                        <p className="font-bold text-white ">Help our cause by donating to the following:</p>
+                        <div className="bg-white p-2 rounded-md mt-2">
+                            <p className="font-bold text-black"> Paybill No</p>
+                            <div className="flex">
+                                
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
