5dba7f3e718e7d7fea53b99b8a8688ee009d3c92
Ayim William <odhisayim@gmail.com>
2024-04-21 19:15:08 +0300

    Finished modal's body

--- components/support-us/donate/modal.tsx
+++ components/support-us/donate/modal.tsx
@@ -9,7 +9,9 @@ export default function Modal() {
                 <>
                     <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
                         <div className="relative w-auto my-6 max-auto max-w-sm">
+                            {/* content */}
                             <div className="border-0 rounded-lg shadpw-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
+                                {/* header */}
                                 <div className="flex items-start justify-between p-5 border-b border-solid border-blueGray-200 rounded-t">
                                     <h3 className="text-3xl font-semibold">
                                         Title
@@ -22,6 +24,12 @@ export default function Modal() {
                                             X
                                         </span>
                                     </button>
+                                </div>
+                                {/* body */}
+                                <div className="relative p-6 flex-auto">
+                                    <p className="my-4 text-blueGray-500 text-lg leading-relaxed">
+                                        something needs to be written here
+                                    </p>
                                 </div>
                             </div>
                         </div>
