dc8f0e9f0ef4e97ee45cf67c23e3887f00efe23b
Ayim William <odhisayim@gmail.com>
2024-04-21 21:00:02 +0300

    fixed some wording

--- components/MpesaCard.tsx
+++ components/MpesaCard.tsx
@@ -4,7 +4,7 @@ const MpesaCard = () => {
             <div className="bg-green-500 text-white text-center py-4 paybill-box">
                 <div className="flex justify-center mb-4">
                     <div className="text-left mr-6">
-                        <p className="font-bold text-white ">Help our cause by donating to the following:</p>
+                        <p className="font-bold text-white ">Your contribution makes a world of difference. Join us in making a difference, empowering communities and transforming lives one act of generosity at a time with your kind donation.</p>
                         <div className="bg-white p-2 rounded-md mt-2">
                             <p className="font-bold text-black"> Paybill No</p>
                             <div className="flex">
