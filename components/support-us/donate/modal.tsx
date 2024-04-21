de6a800a451addbab64cc32bc7a81dfd9e1fcd3e
Ayim William <odhisayim@gmail.com>
2024-04-21 19:42:39 +0300

    Finished footer of Modal

--- components/support-us/donate/modal.tsx
+++ components/support-us/donate/modal.tsx
@@ -31,11 +31,22 @@ export default function Modal() {
                                         something needs to be written here
                                     </p>
                                 </div>
+                                {/* footer */}
+                                <div className="flex items-center justify-end p-6 border-t border-solid border-blueGray-200 rounded-b" >
+                                    <button
+                                        className="text-red-500 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                                        type="button"
+                                        onClick={() => setShowModal(false)}
+                                    >
+                                        Close
+                                    </button>
+                                </div>
                             </div>
                         </div>
                     </div>
+                    <div className="opacity-25 fixed inset-0 z-40 bg-black"></div>
                 </>
             ) : null }
         </>
-    )
+    );
 }
