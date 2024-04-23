1255c6be6c90183f0bbcc50dd1fd02439848561c
Ayim William <odhisayim@gmail.com>
2024-04-23 21:10:52 +0300

    Started a fresh

--- components/MpesaCard.tsx
+++ components/MpesaCard.tsx
@@ -1,55 +1,26 @@
 const MpesaCard = () => {
-    return (
-        <>
-            <div className="bg-green-500 text-white text-center py-4 paybill-box">
-                <div className="flex justify-center mb-4">
-                    <div className="text-left mr-6">
-                        <p className="font-bold text-white ">Your contribution makes a world of difference. Join us in making a difference, empowering communities and transforming lives one act of generosity at a time with your kind donation.</p>
-                        <div className=" p-2 rounded-md mt-2">
-                            <p className="font-bold text-black"> Paybill No</p>
-                            <div className="flex">
-                                <div className="bg-white p-2 rounded-md mr-1">
-                                    <p className="text-black"> 1 </p>
-                                </div>
-                                <div className="bg-white p-2 rounded-md mr-1">
-                                    <p className="text-black">2</p>
-                                </div>
-                                <div className="bg-white p-2 rounded-md mr-1">
-                                    <p className="text-black">3</p>
-                                </div>
-                                <div className="bg-white p-2 rounded-md mr-1">
-                                    <p className="text-black">4</p>
-                                </div>
-                                <div className="bg-white p-2 rounded-md mr-1">
-                                    <p className="text-black">5</p>
-                                </div>
-                                <div className="bg-white p-2 rounded-md mr-1">
-                                    <p className="text-black">6</p>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div className=" p-2 rouded-md">
-                        <p className="font-bold text-black">Account No</p>
-                        <div className="flex">
-                            <div className="bg-white p-2 rounded-md mr-1">
-                                <p className="text-black">7</p>
-                            </div>
-                            <div className="bg-white p-2 rounded-md mr-1">
-                                <p className="text-black">8</p>
-                            </div>
-                            <div className="bg-white p-2 rounded-md mr-1">
-                                <p className="text-black">9</p>
-                            </div>
-                            <div className="bg-white p-2 rounded-md mr-1">
-                                <p className="text-black">0</p>
-                            </div>
-                        </div>
+  return (
+    <>
+      <div className="flex flex-col md:flex-row md:items-center">
+        <div className="md:w-2/3">
+            <div className="bg-green-500 text-white text-center py-4 md:py-8 px-4 md:px-8 mx-auto rounded-lg md:rounded-none">
+                <p className="font-bold text-white mb-4">
+                    Your contribution makes a world of difference. Join us in empowering communities and transforming lives one act at a time with your kind donation. 
+                </p>
+                <div className="flex justify-center">
+                    <div className="p-2 rounded-md">
+                        <p className="font-bold text-black">
+                            Paybill No
+                        </p>
                     </div>
                 </div>
-            </div>
-        </>
-    )
-}
 
-export default MpesaCard 
+            </div>
+
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default MpesaCard;
