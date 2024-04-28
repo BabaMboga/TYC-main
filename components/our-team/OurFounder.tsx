8e3bf04514a5d3c2991355161ba9fad15d33e0a5
Ayim William <odhisayim@gmail.com>
2024-04-29 00:14:50 +0300

    Added OurFounder header

---
+++ components/our-team/OurFounder.tsx
@@ -0,0 +1,15 @@
+import {FC} from "react";
+import Image from "next/image";
+import { Button } from "@/components/ui/button";
+import { Icons } from "@/components/Icons";
+import Link from "next/link";
+
+const OurFounder = () => {
+    return (
+        <div className="relative flex flex-col-reverse items-center md:py-16">
+            <div>
+                <h2></h2>
+            </div>
+        </div>
+    )
+}
