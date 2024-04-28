2e8eeb31e9c450bd569e1b42cfec43fc8a7b34b1
Ayim William <odhisayim@gmail.com>
2024-04-28 16:19:49 +0300

    Made text color change on our Herotext component

--- components/our-team/HeroText.tsx
+++ components/our-team/HeroText.tsx
@@ -2,13 +2,13 @@ const HeroText = () => {
     return (
         <div className="flex flex-col gap-4 px-3 md:px-14 py-10 bg-[#19251D] text-slaye-50">
             <div className="items-center md:px-12">
-                <p className="font-semibold text-center text-xl md:px-2">
+                <p className="font-semibold text-center text-color-white text-xl md:px-2">
                     Meet Our Dedicated Team.
                 </p>
             </div>
 
             <div className="flex items-center md:px-12">
-                <p className="text-xl font-normal text-center md:px-2">
+                <p className="text-xl font-normal text-color-white text-center md:px-2">
                     Our team is comprised of passionate individuals who are committed to making a difference. 
                     With diverse backgrounds and expertise, we come together to provide unwavering support and 
                     guidance to our community. Whether it&apos;s mentoring, coaching, or simply lending a listening 
