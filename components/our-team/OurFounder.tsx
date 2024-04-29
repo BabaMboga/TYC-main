0f93ad163f6c057aead2e03b83ea9e478c7247f3
Ayim William <odhisayim@gmail.com>
2024-04-29 18:55:35 +0300

    Added section to Ourfounder

--- components/our-team/OurFounder.tsx
+++ components/our-team/OurFounder.tsx
@@ -6,10 +6,17 @@ import Link from "next/link";
 
 const OurFounder = () => {
     return (
-        <div className="relative flex flex-col-reverse items-center md:py-16">
-            <div>
-                <h2></h2>
+        <section className="p-2 md:px-10 md:py-8 bg-red-700 md:min-h-[600px]">
+            <div className="relative flex flex-col-reverse items-center md:py-16">
+                <div className="md:mr-[50%] md:w-1/2 md:z-10 border-b-4 border-b-black bg-white md:px-4 relative md:top-6 flex items-center flex-col">
+                    <h2 className="py-3 px-2 text-2xl text-center font-semibold">
+                        CAROLINE W. MUGWE
+                    </h2>
+                    <h2 className="py-3 px-2 text-2xl text-center font-semibold">
+                        FOUNDER/DIRECTOR
+                    </h2>
+                </div>
             </div>
-        </div>
+        </section>
     )
 }
