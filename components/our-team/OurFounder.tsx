d2b2c22a027686c161138cc1b586be3ee4795146
Ayim William <odhisayim@gmail.com>
2024-04-29 19:53:55 +0300

    Finished making corrections

--- components/our-team/OurFounder.tsx
+++ components/our-team/OurFounder.tsx
@@ -15,6 +15,17 @@ const OurFounder = () => {
                     <h2 className="py-3 px-2 text-2xl text-center font-semibold">
                         FOUNDER/DIRECTOR
                     </h2>
+                    <p className="px-3 py-3 text-base font-normal text-center">
+                        Caroline is a highly motivated Kenyan, a human resource professional, a Certified Secretary and a 
+                        Chartered Mediator with a first degree in Commerce( University of Nairobi, 1987 ) and a Master&apos;s 
+                        degree in Business Administration. She has over thirty years of experience in human capital & administration
+                        and is a team builder who is also very strong on governance issues. She has served on many Parastatal 
+                        Boards within the Government of Kenya and she is also highly involved in Community interventions especially
+                        touching on youth and vulnerable members of the society. Carole has undergone capacity building through training 
+                        locally and abroad, thus acquiring skills and competencies that catapulted her into top leadership. She is passionate 
+                        about using her skills to help people to become better and as she says, she is happily value-driven. She is a mother 
+                        of two children. 
+                    </p>
                 </div>
             </div>
         </section>
