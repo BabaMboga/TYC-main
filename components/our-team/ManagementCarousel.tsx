7ca419c2ceaaca01a7d7e51c2ad10c1c2741d07c
Ayim William <odhisayim@gmail.com>
2024-05-02 16:36:32 +0300

    Added data

--- components/our-team/ManagementCarousel.tsx
+++ components/our-team/ManagementCarousel.tsx
@@ -1,8 +1,28 @@
 const managementData = [
     {
-        name: '',
+        name: 'Captain Peter M. Maranga',
         img: '',
-        description: ''
+        description: 'A veteran pilot and aviator, who has flown with the Kenya Air Force, worked in the Directorate of Civil Aviation and flow as a Captain of the Boeing 737, Boeing 767, Boeing 777 & Boeing 787 at Kenya Airways for many years.'
+    },
+    {
+        name: "Teresa W. Thiong'o ",
+        img: '',
+        description: 'Counselling Pyschologist for Children and adolescents with speciality in trauma and loss & grief. HRM specialist. Founder & director Channan Jotis counselling and Rehabilitation Center.'
+    },
+    {
+        name: 'Mrs Rose M Wanjohi',
+        img: '',
+        description: 'Director of human resource and admin, Royal Media Services'
+    },
+    {
+        name: 'Rev Dr. James C. Gathioro',
+        img: '',
+        description: 'A pilot, reverend and scholar'
+    },
+    {
+        name: 'Mrs. Jayne M. Gatonye',
+        img: '',
+        description: 'Bcom, MBA, entrepreneur & motivational speaker. Has climbed Mt.Kenya, Mt.Kilimanjaro & Mt.Everest. She comes to encourage young people to be disciplined and resilient.'
     },
     {
         name: '',
@@ -29,24 +49,6 @@ const managementData = [
         img: '',
         description: ''
     },
-    {
-        name: '',
-        img: '',
-        description: ''
-    },
-    {
-        name: '',
-        img: '',
-        description: ''
-    },
-    {
-        name: '',
-        img: '',
-        description: ''
-    },
-    {
-        name: '',
-        img: '',
-        description: ''
-    },
-]
+];
+
+export default managementData;
