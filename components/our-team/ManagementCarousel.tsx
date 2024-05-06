3c5e488494c98cd6e5f087c9b7742756573d4226
Ayim William <odhisayim@gmail.com>
2024-05-06 17:53:41 +0300

    fixed react-slick issue

--- components/our-team/ManagementCarousel.tsx
+++ components/our-team/ManagementCarousel.tsx
@@ -2,3 +2,20 @@ import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
+function ManagementCarousel() {
+    const settings = {
+        dots: true,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+    };
+
+    return (
+        <div>
+            <div>
+                <Slider {...settings}></Slider>
+            </div>
+        </div>
+    )
+}
