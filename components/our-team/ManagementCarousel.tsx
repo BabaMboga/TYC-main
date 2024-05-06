dde666b9f2626e6b5b619ea19055d22b96b0d28c
Ayim William <odhisayim@gmail.com>
2024-05-06 18:27:08 +0300

    Added more divs on card

--- components/our-team/ManagementCarousel.tsx
+++ components/our-team/ManagementCarousel.tsx
@@ -1,6 +1,7 @@
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+import managementData from "./ManagementData";
 
 function ManagementCarousel() {
     const settings = {
@@ -12,9 +13,37 @@ function ManagementCarousel() {
     };
 
     return (
-        <div>
-            <div>
-                <Slider {...settings}></Slider>
+        <div className="w-3/4 m-auto">
+            <div className="mt-20">
+                <Slider {...settings}>
+                    {managementData.map((data) => (
+                        <div key={data.name}>
+                            <a href="#" className="group relative block bg-black">
+                                <img 
+                                    alt=""
+                                    src={data.img}
+                                    className="absolute inset-0 h-full w-full object-cover opacity-75 transition-opacity group-hover:opacity-50"
+                                />
+
+                                <div className="relative p-4 sm:p-6 lg:p-8">
+                                    <p className="text-sm font-medium uppercase tracking-wideset text-pink-500">
+                                        Facilitator
+                                    </p>
+                                    <p className="text-xl font-bold text-white sm:text-2xl">
+                                        {data.name}
+                                    </p>
+                                    <div>
+                                        <div>
+                                            <p>
+                                                {data.description}
+                                            </p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </a>
+                        </div>
+                    ))}
+                </Slider>
             </div>
         </div>
     )
