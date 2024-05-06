0ef551f7b793fd70ded2cfc6b9d0e962ed23d1fc
Ayim William <odhisayim@gmail.com>
2024-05-06 20:02:47 +0300

    Finished card

--- components/our-team/ManagementCarousel.tsx
+++ components/our-team/ManagementCarousel.tsx
@@ -32,9 +32,9 @@ function ManagementCarousel() {
                                     <p className="text-xl font-bold text-white sm:text-2xl">
                                         {data.name}
                                     </p>
-                                    <div>
-                                        <div>
-                                            <p>
+                                    <div className="mt-32 sm:mt-48 lg:mt-64">
+                                        <div className="translate-y-8 tranform opacity-0 transition-all group-hover:translate-y-0 group-hover:opacity-100">
+                                            <p className="text-sm text-white">
                                                 {data.description}
                                             </p>
                                         </div>
