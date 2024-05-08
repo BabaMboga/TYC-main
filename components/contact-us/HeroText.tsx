8eb03fd1b01afb122b6d49e93bd8cfa7aa50dd8d
Ayim William <odhisayim@gmail.com>
2024-05-08 18:46:28 +0300

    Added paragraph to herotext

--- components/contact-us/HeroText.tsx
+++ components/contact-us/HeroText.tsx
@@ -1,8 +1,13 @@
 const HeroText = () => {
     return (
-        <div>
-            <div>
-                <p></p>
+        <div className="flex items-start flex-col gap-4 px-3 md:px-14 py-10 bg-[#19251D] text-slate-50">
+            <div className="flex items-center md:px-12">
+                <p className="text-base md:text-lg font-normal text-center md:px-2">
+                    Ready to take the next step? We&apos;re here to listen, assist and guide you. Whether you have questions, 
+                    feedback or would like to explore partnership opportunities,our team is dedicated to providing you with 
+                    the support you need. Reach out to us today and let's start a conversation about how we can work together 
+                    to create positive change in our communities.
+                </p>
             </div>
         </div>
     )
