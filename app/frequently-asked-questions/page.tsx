79920c8504ee41399ceb740bf2fbddd99cfadb48
Ayim William <odhisayim@gmail.com>
2024-05-19 01:05:27 +0300

    Added components to FAQ page

--- app/frequently-asked-questions/page.tsx
+++ app/frequently-asked-questions/page.tsx
@@ -1,6 +1,12 @@
+import HeroText from "@/components/frequently-asked-questions/HeroText";
+import FAQ from "@/components/frequently-asked-questions/FAQ";
+
 const Home = ({}) => {
     return (
         <>
+            <HeroText />
+
+            <FAQ />
         </>
 
     );
--- components/frequently-asked-questions/FAQ.tsx
+++ components/frequently-asked-questions/FAQ.tsx
@@ -106,3 +106,5 @@ const FrequentlyAskedQuestions = () => {
         </>
     );
 };
+
+export default FrequentlyAskedQuestions;
