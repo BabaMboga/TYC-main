b23ffe8161f13a3c0f3af299214ba4b2fae471ac
Ayim William <odhisayim@gmail.com>
2024-05-18 21:11:28 +0300

    Added first paragraph

---
+++ components/frequently-asked-questions/HeroText.tsx
@@ -0,0 +1,13 @@
+import { FC } from "react";
+
+const HeroText = () => {
+    return (
+        <div className="flex flex-col gap-4 px-3 ,d:px-14 py-10 bg-[#19251D] text-slate-50">
+            <div className="items-center md:px-12">
+                <p className="font-semibold text-center text-xl md:px-2">
+                    Frequently Asked Questions
+                </p>
+            </div>
+        </div>
+    )
+}
