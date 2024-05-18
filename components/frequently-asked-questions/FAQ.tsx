f0ade45199aaebb1066eb6918ef051eeedc2f03a
Ayim William <odhisayim@gmail.com>
2024-05-19 00:42:20 +0300

    Added a third question

--- components/frequently-asked-questions/FAQ.tsx
+++ components/frequently-asked-questions/FAQ.tsx
@@ -67,7 +67,42 @@ const FrequentlyAskedQuestions = () => {
                         </a> 
                     </p>
                 </details>
+
+                <details 
+                    className="group border-s-4 border-green-500 bg-gray-50 p-6 [&_summary::-webkit-details-marker]:hidden"
+                    open
+                >
+                    <summary className="flex cursor-pointer items-center justify-between gap-1.5">
+                        <h2 className="text-lg font-medium text-gray-900">
+                            How can I volunteer with TYC AFRICA TRUST?
+                        </h2>
+
+                        <span className="shrink-0 rounded-full bg-white p-1.5 text-gray-900 sm:p-3">
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                className="size-5 shriink-0 transition duration-300 group-open:-rotate-45"
+                                viewBox="0 0 20 20"
+                                fill="currentColor"
+                            >
+                                <path
+                                    fillRule="evenodd"
+                                    d="M10 3a1 1 0 011 1v5h5a1 1 0 110 2h-5v5a1 1 0 11-2 0v-5H4a1 1 0 110-2h5V4a1 1 0 011-1z"
+                                    clipRule="evenodd" 
+                                />
+                            </svg>
+                        </span>
+                    </summary>
+
+                    <p className="mt-4 leading-relaxed text-gray-700">
+                        Please register your area of interest{" "}
+                        <a href="">
+                            <span className="text-gray-700 text-xl font-bold underline">
+                                here.
+                            </span>
+                        </a> 
+                    </p>
+                </details>
             </div>
         </>
-    )
-}
+    );
+};
