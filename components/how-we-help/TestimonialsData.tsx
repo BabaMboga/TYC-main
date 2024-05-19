0c08bd206ede3fc854f23ca0630ad4f009e6a4d6
Ayim William <odhisayim@gmail.com>
2024-05-19 18:13:38 +0300

    Added TestimonialData

---
+++ components/how-we-help/TestimonialsData.tsx
@@ -0,0 +1,6 @@
+const testimonials = [
+    {
+        name: "John Doe",
+        testimonial: "This organisation changed my life. The support and resources provided helped me gain confidence and develop essential skills."
+    }
+]
