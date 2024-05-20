ca0e2a4a6fcf6341a3f7a29fb299b1ea29a50b53
Ayim William <odhisayim@gmail.com>
2024-05-20 07:11:18 +0300

    Added two more questions

--- components/frequently-asked-questions/FAQ.tsx
+++ components/frequently-asked-questions/FAQ.tsx
@@ -102,6 +102,72 @@ const FrequentlyAskedQuestions = () => {
                         </a> 
                     </p>
                 </details>
+
+                <details 
+                    className="group border-s-4 border-green-500 bg-gray-50 p-6 [&_summary::-webkit-details-marker]:hidden"
+                    open
+                >
+                    <summary className="flex cursor-pointer items-center justify-between gap-1.5">
+                        <h2 className="text-lg font-medium text-gray-900">
+                            How does TYC AFRICA TRUST support youth development?
+                        </h2>
+
+                        <span className="shrink-0 rounded-full bg-white p-1.5 text-gray-900 sm:p-3">
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                className="size-5 shriink-0 transition duration-300 group-open:-rotate-45"
+                                viewBox="0 0 20 20"
+                                fill="currentColor"
+                            >
+                                <path
+                                    fillRule="evenodd"
+                                    d="M10 3a1 1 0 011 1v5h5a1 1 0 110 2h-5v5a1 1 0 11-2 0v-5H4a1 1 0 110-2h5V4a1 1 0 011-1z"
+                                    clipRule="evenodd" 
+                                />
+                            </svg>
+                        </span>
+                    </summary>
+
+                    <p className="mt-4 leading-relaxed text-gray-700">
+                        TYC supports youth development through a variety of comprehensive programs and 
+                        initiatives aimed at empowering young people and enhancing their personal and 
+                        professional growth via ways such as mentorship programs, educational workshops, 
+                        community engagement, career development, mental health support, networking opportunities 
+                        among others.
+                    </p>
+                </details>
+
+                <details 
+                    className="group border-s-4 border-green-500 bg-gray-50 p-6 [&_summary::-webkit-details-marker]:hidden"
+                    open
+                >
+                    <summary className="flex cursor-pointer items-center justify-between gap-1.5">
+                        <h2 className="text-lg font-medium text-gray-900">
+                            How can I donate?
+                        </h2>
+
+                        <span className="shrink-0 rounded-full bg-white p-1.5 text-gray-900 sm:p-3">
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                className="size-5 shriink-0 transition duration-300 group-open:-rotate-45"
+                                viewBox="0 0 20 20"
+                                fill="currentColor"
+                            >
+                                <path
+                                    fillRule="evenodd"
+                                    d="M10 3a1 1 0 011 1v5h5a1 1 0 110 2h-5v5a1 1 0 11-2 0v-5H4a1 1 0 110-2h5V4a1 1 0 011-1z"
+                                    clipRule="evenodd" 
+                                />
+                            </svg>
+                        </span>
+                    </summary>
+
+                    <p className="mt-4 leading-relaxed text-gray-700">
+                        Visit the donate page on our website to access our various ways in which one can donate to our cause.
+                    </p>
+                </details>
+
+
             </div>
         </>
     );
