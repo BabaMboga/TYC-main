e4e8bb47b51f7dfd06e55a80fd53349eb225eb95
Ayim William <odhisayim@gmail.com>
2024-05-20 08:08:08 +0300

    Formatted insights card

--- components/resources/Insights.tsx
+++ components/resources/Insights.tsx
@@ -8,46 +8,58 @@ const Insights = ({}) => {
     <>
       <div className=" relative flex flex-col justify-start items-center gap-2.5 ">
         <div className="flex flex-col items-center justify-start gap-5 p-2 text-white md:gap-16 md:p-20 grow shrink basis-0">
-          
           <div className="flex flex-col items-start justify-start gap-12 p-2 ">
             <div className="flex flex-col items-center justify-start gap-5 ">
               <div className=" text-center text-2xl  md:text-[42px] font-bold  ">
                 Blogs and Insights
               </div>
               <div className=" text-center text-lg  md:text-[22px]   ">
-               <p>Dive into our regularly updated blog, where we share insightful articles, stories, and 
-advice. Our blog covers a spectrum of topics related to youth development, mental 
-wellness, community impact, and more.</p>
+                <p>
+                  Dive into our regularly updated blog, where we share
+                  insightful articles, stories, and advice. Our blog covers a
+                  spectrum of topics related to youth development, mental
+                  wellness, community impact, and more.
+                </p>
               </div>
             </div>
           </div>
 
           <div className="grid gap-4 md:grid-cols-3">
-
             <div className="  border border-zinc-200 flex-col   gap-[5px] inline-flex">
-             
-              <Image src={"/images/photo6.jpg"} alt={""} height={400} width={400} sizes="100vw"
-              // Make the image display full width
-              style={{
-                width: '100%',
-                height: '400px',}}></Image>
+              <Image
+                src={"/images/photo6.jpg"}
+                alt={""}
+                height={400}
+                width={400}
+                sizes="100vw"
+                // Make the image display full width
+                style={{
+                  width: "100%",
+                  height: "400px",
+                }}
+              ></Image>
               <div className="flex flex-col items-start self-stretch justify-start gap-4 px-4 pt-6 pb-4">
                 <div className="flex flex-col items-start justify-start gap-1 ">
                   <div className="self-stretch  text-[40px] font-bold  leading-[44px]">
-                   Success Stories
+                    Success Stories
                   </div>
                 </div>
                 <div className="self-stretch text-2xl font-normal  leading-[33.60px]">
-                Be inspired by success stories of individuals who have 
-thrived through TYC Africa Trust&apos;s programs. Learn from their journeys and 
-discover the possibilities that await you.
+                  Be inspired by success stories of individuals who have thrived
+                  through TYC Africa Trust&apos;s programs. Learn from their
+                  journeys and discover the possibilities that await you.
                 </div>
               </div>
               <div className=" gap-4 p-4 ">
                 <div className=" gap-4 py-4 pr-2 ">
                   <div className="">
-                  <div className="text-xl font-medium leading-tight tracking-wide underline ">
-                     <Link href={'/'} className="flex flex-row justify-between w-full"><p>Learn more</p> <Icons.right/></Link>
+                    <div className="text-xl font-medium leading-tight tracking-wide underline ">
+                      <Link
+                        href={"/"}
+                        className="flex flex-row justify-between w-full"
+                      >
+                        <p>Learn more</p> <Icons.right />
+                      </Link>
                     </div>
                   </div>
                   <div className="relative w-6 h-6" />
@@ -56,12 +68,18 @@ discover the possibilities that await you.
             </div>
 
             <div className="  border border-zinc-200 flex-col  gap-[5px] inline-flex ">
-              
-              <Image  alt={""} src={"/images/cardpicture3.png"} height={400} width={400} sizes="100vw"
-              // Make the image display full width
-              style={{
-                width: '100%',
-                height: '400px',}}></Image>
+              <Image
+                alt={""}
+                src={"/images/cardpicture3.png"}
+                height={400}
+                width={400}
+                sizes="100vw"
+                // Make the image display full width
+                style={{
+                  width: "100%",
+                  height: "400px",
+                }}
+              ></Image>
 
               <div className="flex flex-col items-start self-stretch justify-start gap-4 px-4 pt-6 pb-4">
                 <div className="flex flex-col items-start justify-start gap-1 ">
@@ -70,10 +88,9 @@ discover the possibilities that await you.
                   </div>
                 </div>
                 <div className="self-stretch text-2xl font-normal  leading-[33.60px]">
-                Gain valuable insights from experts in various fields. Our blog 
-features expert advice on personal development, mental health, and 
-strategies for success.
-                  
+                  Gain valuable insights from experts in various fields. Our
+                  blog features expert advice on personal development, mental
+                  health, and strategies for success.
                 </div>
               </div>
 
@@ -81,7 +98,12 @@ strategies for success.
                 <div className=" gap-4 py-4 pr-2 ">
                   <div className=" ">
                     <div className="text-xl font-medium leading-tight tracking-wide underline ">
-                     <Link href={'/'} className="flex flex-row justify-between w-full"><p>Learn more</p> <Icons.right/></Link>
+                      <Link
+                        href={"/"}
+                        className="flex flex-row justify-between w-full"
+                      >
+                        <p>Learn more</p> <Icons.right />
+                      </Link>
                     </div>
                   </div>
                   <div className="relative w-6 h-6" />
@@ -90,14 +112,19 @@ strategies for success.
             </div>
 
             <div className="  border border-zinc-200 flex-col  gap-[5px] inline-flex ">
-             
+              <Image
+                alt={""}
+                src={"/images/photo4.jpg"}
+                height={400}
+                width={400}
+                sizes="100vw"
+                // Make the image display full width
+                style={{
+                  width: "100%",
+                  height: "400px",
+                }}
+              ></Image>
 
-              <Image  alt={""} src={"/images/photo4.jpg"} height={400} width={400} sizes="100vw"
-              // Make the image display full width
-              style={{
-                width: '100%',
-                height: '400px',}}></Image>
-              
               <div className="flex flex-col items-start self-stretch justify-start gap-4 px-4 pt-6 pb-4">
                 <div className="flex flex-col items-start justify-start gap-1 ">
                   <div className="self-stretch  text-[40px] font-bold  leading-[44px]">
@@ -105,23 +132,27 @@ strategies for success.
                   </div>
                 </div>
                 <div className="self-stretch text-2xl font-normal  leading-[33.60px]">
-                Hear directly from youth about their experiences, 
-challenges, and triumphs. Connect with the voices that resonate with your 
-own aspirations and dreams.
+                  Hear directly from youth about their experiences, challenges,
+                  and triumphs. Connect with the voices that resonate with your
+                  own aspirations and dreams.
                 </div>
               </div>
               <div className=" gap-4 p-4 ">
                 <div className=" h-12 py-4 pr-2 grow shrink basis-0">
                   <div className="">
-                  <div className="text-xl font-medium leading-tight tracking-wide underline ">
-                     <Link href={'/'} className="flex flex-row justify-between w-full"><p>Learn more</p> <Icons.right/></Link>
+                    <div className="text-xl font-medium leading-tight tracking-wide underline ">
+                      <Link
+                        href={"/"}
+                        className="flex flex-row justify-between w-full"
+                      >
+                        <p>Learn more</p> <Icons.right />
+                      </Link>
                     </div>
                   </div>
                   <div className="relative w-6 h-6" />
                 </div>
               </div>
             </div>
-
           </div>
         </div>
       </div>
