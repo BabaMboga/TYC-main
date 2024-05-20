bee74ca73268ffb3d99967f2e4d63e4f4b311078
Ayim William <odhisayim@gmail.com>
2024-05-20 07:32:37 +0300

    Added last batch of questions

--- components/frequently-asked-questions/FAQ.tsx
+++ components/frequently-asked-questions/FAQ.tsx
@@ -260,6 +260,68 @@ const FrequentlyAskedQuestions = () => {
                     </p>
                 </details>
 
+                <details 
+                    className="group border-s-4 border-green-500 bg-gray-50 p-6 [&_summary::-webkit-details-marker]:hidden"
+                    open
+                >
+                    <summary className="flex cursor-pointer items-center justify-between gap-1.5">
+                        <h2 className="text-lg font-medium text-gray-900">
+                            What type of events does TYC AFRICA TRUST host?
+                        </h2>
+
+                        <span className="shrink-0 rounded-full bg-white p-1.5 text-gray-900 sm:p-3">
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                className="size-5 shriink-0 transition duration-300 group-open:-rotate-45"
+                                viewBox="0 0 20 20"
+                                fill="currentColor"
+                            >
+                                <path
+                                    fillRule="evenodd"
+                                    d="M10 3a1 1 0 011 1v5h5a1 1 0 110 2h-5v5a1 1 0 11-2 0v-5H4a1 1 0 110-2h5V4a1 1 0 011-1z"
+                                    clipRule="evenodd" 
+                                />
+                            </svg>
+                        </span>
+                    </summary>
+
+                    <p className="mt-4 leading-relaxed text-gray-700">
+                        We host workshops, seminars, webinars, conferences and community events such 
+                        as Love Visits focused on personal development, career skills, mental health 
+                        awareness and so much more.
+                    </p>
+                </details>
+
+                <details 
+                    className="group border-s-4 border-green-500 bg-gray-50 p-6 [&_summary::-webkit-details-marker]:hidden"
+                    open
+                >
+                    <summary className="flex cursor-pointer items-center justify-between gap-1.5">
+                        <h2 className="text-lg font-medium text-gray-900">
+                            Can I bring a friend to your events?
+                        </h2>
+
+                        <span className="shrink-0 rounded-full bg-white p-1.5 text-gray-900 sm:p-3">
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                className="size-5 shriink-0 transition duration-300 group-open:-rotate-45"
+                                viewBox="0 0 20 20"
+                                fill="currentColor"
+                            >
+                                <path
+                                    fillRule="evenodd"
+                                    d="M10 3a1 1 0 011 1v5h5a1 1 0 110 2h-5v5a1 1 0 11-2 0v-5H4a1 1 0 110-2h5V4a1 1 0 011-1z"
+                                    clipRule="evenodd" 
+                                />
+                            </svg>
+                        </span>
+                    </summary>
+
+                    <p className="mt-4 leading-relaxed text-gray-700">
+                        Yes, you are welcomed to bring friends to our events. Some events may require 
+                        registration, so please check the event details.
+                    </p>
+                </details>
 
 
             </div>
