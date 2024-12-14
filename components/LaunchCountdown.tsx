f935ed0c62bee2959a50485856f638a16c8321de
Ayim William <odhisayim@gmail.com>
2024-12-14 07:25:05 +0300

    finished working on countdown

--- components/LaunchCountdown.tsx
+++ components/LaunchCountdown.tsx
@@ -10,22 +10,22 @@ const itim = Itim({weight: ["400"], subsets: ["latin"]});
 const LaunchCountdown = () => {
   return (
     <main className="bg-yellow-400 container mx-auto h-screen flex justify-center items-center">
-      <section className="px-4 w-full flex justify-between items-center">
-        <aside className="w-1/2">
-          <h1 className='text-8xl mb-5 text-black'>
+      <section className="px-4 w-full flex flex-col lg:flex-row justify-between items-center">
+        <aside className="w-full lg:w-1/2 text-center lg:text-start">
+          <h1 className='text-6xl lg:text-7xl xl:text-8xl mb-5 text-black'>
             <small className='text-3xl font-semibold'>Our Website is</small>
             <br />
             <span className={itim.className}>Launching Soon</span>
           </h1>
-          <h2 className='text-black text-xl'>
+          <h2 className='text-black text-lg lg:text-xl'>
             We&apos;re{" "} 
             <span className='bg-white px-2 py-1 font-semibold rounded shadow-md'>Under Construction</span> will be here soon.
           </h2>
-          <h3 className='bg-white text-black font-semibold text-2xl uppercase px-2 py-1 inline-block rounded shadow-md mt-5'>Stay Tuned!</h3>
-          <Timer />
+          <h3 className='bg-white text-black font-semibold text-xl lg:text-2xl uppercase px-2 py-1 inline-block rounded shadow-md mt-5'>Stay Tuned!</h3>
+          <Timer launchDate='2024-14-12T10:00:00'/>
           
         </aside>
-        <aside className="w-1/2 flex justify-center pl-4">
+        <aside className="w-full lg:w-1/2 flex justify-center mt-5 lg:mt-0 lg:pl-4">
           <Image 
             src = "/funny-launch.png"
             alt = "developers at work"
