05101959433b5bce11f6cd9342e6bbae83d8bc43
Ayim William <odhisayim@gmail.com>
2025-11-07 09:09:14 +0300

    added community outreach and reselience building

--- components/how-we-help/OpportunitiesCard1.tsx
+++ components/how-we-help/OpportunitiesCard1.tsx
@@ -118,7 +118,7 @@ const OpportunitiesCard1 = ({}) => {
                 <div className=" gap-4 py-4 pr-2 ">
                   <div className=" ">
                     <div className="text-base font-medium leading-tight tracking-wide underline flex justify-between w-full">
-                      <Link className="flex" href={"/how-we-help"}>
+                      <Link className="flex" href={"/how-we-help/resilience-building"}>
                         Learn more <Icons.right />
                       </Link>
                     </div>
@@ -158,7 +158,7 @@ const OpportunitiesCard1 = ({}) => {
                 <div className=" gap-4 py-4 pr-2 ">
                   <div className=" ">
                     <div className="text-base font-medium leading-tight tracking-wide underline flex justify-between w-full">
-                      <Link className="flex" href={"/how-we-help"}>
+                      <Link className="flex" href={"/how-we-help/community-outreach"}>
                         Learn more <Icons.right />
                       </Link>
                     </div>
