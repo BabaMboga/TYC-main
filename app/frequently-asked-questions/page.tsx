b9a2285acf5e94f5c61890594527cfe8407f21fe
Ayim William <odhisayim@gmail.com>
2025-11-18 19:58:04 +0300

    feat:added survey below faqs

--- app/frequently-asked-questions/page.tsx
+++ app/frequently-asked-questions/page.tsx
@@ -1,6 +1,6 @@
 import HeroText from "@/components/frequently-asked-questions/HeroText";
 import FAQ from "@/components/frequently-asked-questions/FAQ";
-import OurSurvey from "@/components/frequently-asked-questions/OurTeam";
+import OurSurvey from "@/components/frequently-asked-questions/OurSurvey";
 
 const Home = ({}) => {
     return (
@@ -9,7 +9,7 @@ const Home = ({}) => {
 
             <FAQ />
 
-            <section className='p-2 md:px-10 md:py-8 bg-[#0E3930] md:min-h-[600px]'>
+            <section className='p-2 md:px-10 md:py-8 bg-[#0E3930] md:min-h-[400px]'>
                 <div className=''>
                     <OurSurvey/>
                 </div>
